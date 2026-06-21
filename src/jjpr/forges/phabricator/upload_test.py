139ae39927b0e87375004b424b9b8970b79885bf
Shish <shish@shishnet.org>
2026-06-21 10:43:10 +0100

    test for pushing a single commit without its parent

--- src/jjpr/forges/phabricator/upload_test.py
+++ src/jjpr/forges/phabricator/upload_test.py
@@ -52,3 +52,17 @@ class TestUpload:
         assert js[0]["title"]["text"] == "Test commit 2"
         assert js[1]["title"]["text"] == "Test commit 1"
         # todo: assert two has one as a parent
+
+    def test_push_two_skipping_one(self, clone: Path):
+        (clone / "test_file.txt").write_text("Test content")
+        run_cmd("jj", "commit", "-m", "Test commit 1")
+
+        (clone / "test_file2.txt").write_text("Test content 2")
+        run_cmd("jj", "commit", "-m", "Test commit 2")
+
+        run_cmd("jj", "pr", "push", "@")
+
+        js = json.loads(run_cmd("jj", "pr", "--format", "json", "list"))
+        assert len(js) == 1
+        assert js[0]["title"] == "Test commit 2"
+        # todo: assert two has no parents
