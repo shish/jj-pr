fdcf3ebc7bfa58231c713c21c3c05491969890f5
Shish <shish@shishnet.org>
2026-08-10 23:06:23 +0100

    [wip][gerrit] test rebase
    Change-Id: Iea09ae81817aba577590716ff454970d6a6a6964

--- src/jjpr/forges/gerrit/rebase_test.py
+++ src/jjpr/forges/gerrit/rebase_test.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from textwrap import dedent
 
 import httpx
 import pytest
 
 from ...conftest import run_cmd, tmp_cwd
 from ...utils import jj
+from .lib.client import GerritClient
 
 pytestmark = [pytest.mark.integration, pytest.mark.gerrit]
 
@@ -22,25 +24,70 @@ class TestRebase:
         output = run_cmd("jj", "pr", "rebase")
         assert "Rebasing" in output
 
-    def test_rebase_onto_updated_main(self, clone: Path, repo: httpx.URL):
-        pytest.skip(
-            "Skipping test because I can't figure out how to allow pushing "
-            "direct to `main` on the test repo o.o"
+    def test_rebase_onto_updated_main(
+        self, session: GerritClient, repo_name: str, clone: Path, repo: httpx.URL
+    ):
+        # enable force-pushes
+        session.post(
+            f"projects/{repo_name}/access",
+            json={
+                "add": {
+                    "refs/heads/*": {
+                        "permissions": {
+                            "push": {
+                                "rules": {
+                                    "global:Registered-Users": {
+                                        "action": "ALLOW",
+                                        "force": True,
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
         )
 
+        # Create a change and upload for review
         (clone / "test_file.txt").write_text("Test content")
         run_cmd("jj", "commit", "-m", "Test commit 1")
         run_cmd("jj", "pr", "upload")
+        assert jj.diagram() == dedent("""
+            @
+            o  Test commit 1
+            +  Initial empty repository
+        """)
 
+        # While the change is being reviewed, `main` moves forwards
         with tmp_cwd() as admin_clone:
-            run_cmd("git", "clone", str(repo), ".")
+            run_cmd("jj", "git", "clone", str(repo), ".")
             (admin_clone / "main_advance.txt").write_text("Main advance content")
             run_cmd("git", "add", ".")
             run_cmd("git", "commit", "-m", "Advance main")
-            run_cmd("git", "push", "origin", "HEAD:refs/heads/main", cap=False)
+            run_cmd(
+                "git", "push", "--force", "origin", "HEAD:refs/heads/main", cap=False
+            )
+            assert jj.diagram() == dedent("""
+                @
+                +  Advance main
+                +  Initial empty repository
+            """)
 
-        run_cmd("jj", "git", "fetch", "origin")
-        output = run_cmd("jj", "pr", "rebase")
-        assert "Rebasing" in output
-        assert jj.description_of("@") == "Test commit 1"
-        assert jj.description_of("@-") == "Advance main"
+        # Fetch to see the new main has moved forwards
+        run_cmd("jj", "git", "fetch", cap=False)
+        assert jj.diagram() == dedent("""
+            @
+            o  Test commit 1
+            | o  Advance main
+            |/
+            +  Initial empty repository
+        """)
+
+        # Rebase on top of the new main
+        run_cmd("jj", "pr", "rebase")
+        assert jj.diagram() == dedent("""
+            @
+            o  Test commit 1
+            o  Advance main
+            +  Initial empty repository
+        """)
