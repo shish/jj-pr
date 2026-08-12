9643e82f87a2512d94ecc27281b87388de3454a1
Shish <shish@shishnet.org>
2026-08-12 01:28:35 +0100

    have remove_ansi also handle osc8
    Change-Id: Ib3fb9cb275506a43231b3f96a3e7a4436a6a6964

--- src/jjpr/utils/text.py
+++ src/jjpr/utils/text.py
@@ -12,5 +12,8 @@ def rich_str(*obj) -> str:
 
 
 def remove_ansi(line):
-    ansi_escape = re.compile(r"(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]")
+    ansi_escape = re.compile(
+        r"\x1B\][^\x07\x1B]*(?:\x07|\x1B\\)"  # OSC sequences (e.g. hyperlinks)
+        r"|(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]"  # CSI and other two-byte sequences
+    )
     return ansi_escape.sub("", line)
--- src/jjpr/utils/text_test.py
+++ src/jjpr/utils/text_test.py
@@ -4,3 +4,20 @@ from . import text
 class TestRemoveAnsi:
     def test_strip(self) -> None:
         assert text.remove_ansi("\x1b[31mHello\x1b[0m") == "Hello"
+
+
+class TestRichStrRoundTrip:
+    def test_color(self) -> None:
+        rendered = text.rich_str("[red]hello[/red]")
+        assert "\x1b" in rendered
+        assert text.remove_ansi(rendered) == "hello"
+
+    def test_bold(self) -> None:
+        rendered = text.rich_str("[bold]hello[/bold]")
+        assert "\x1b" in rendered
+        assert text.remove_ansi(rendered) == "hello"
+
+    def test_link(self) -> None:
+        rendered = text.rich_str("[link=http://example.com]click here[/link]")
+        assert "\x1b" in rendered
+        assert text.remove_ansi(rendered) == "click here"
